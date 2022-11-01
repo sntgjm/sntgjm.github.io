bee1c64a767f38922364cb0295de6e3e95e40e8a
sntgjm <jm2001santiago@gmail.com>
2022-11-01 16:44:11 +0800

    50% with picture still not usable on desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,18 @@ if(navToggle) {
         navMenu.classList.add('show-menu')
     })
 }
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
